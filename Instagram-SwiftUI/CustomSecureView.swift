4c79ece443fd7cb1e5a5e535d754805e20787178
Leonardo Viana <sonleo2013@gmail.com>
2021-01-21 18:40:57 -0400

    chore: create custom secure view

---
+++ Instagram-SwiftUI/CustomSecureView.swift
@@ -0,0 +1,35 @@
+//
+//  CustomSecureView.swift
+//  Instagram-SwiftUI
+//
+//  Created by Leonardo Viana on 21/01/21.
+//
+
+import SwiftUI
+
+struct CustomSecureView: View {
+    var placeholder: String = ""
+    @Binding var text: String
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            if text.isEmpty {
+                Text(placeholder).foregroundColor(Color("PlaceholderColor")).padding()
+            }
+            
+            SecureField("", text: $text)
+                .padding(.horizontal, 20)
+                .foregroundColor(.white)
+            
+        }.overlay(
+            CustomRoundedRectangle().body
+                .stroke(Color("TextFieldBorderColor"), lineWidth: 3)
+        ).background(CustomRoundedRectangle().body.fill(Color("TextFieldColor")))
+    }
+}
+
+struct CustomSecureView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomSecureView(placeholder: "Password", text: .constant("")).previewLayout(.sizeThatFits)
+    }
+}
