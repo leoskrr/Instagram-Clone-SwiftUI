1e900794ef341bebcfa9d633cf2d9cd726e94808
Leonardo Viana <sonleo2013@gmail.com>
2021-01-21 11:28:29 -0400

    Initial Commit

---
+++ Instagram-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instagram-SwiftUI
+//
+//  Created by Leonardo Viana on 21/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
