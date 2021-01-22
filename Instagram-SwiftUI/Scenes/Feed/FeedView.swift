f26a7766fe3fc93ae74880c3e4fcb265d99a4793
Leonardo Viana <sonleo2013@gmail.com>
2021-01-22 09:34:32 -0400

    ref(feed): change name of file and class

--- Instagram-SwiftUI/Scenes/Feed/FeedView.swift
+++ Instagram-SwiftUI/Scenes/Feed/FeedView.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct FeedView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct FeedView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        FeedView()
     }
 }
