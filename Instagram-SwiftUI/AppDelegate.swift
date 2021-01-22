b69c7bd057009b07b62b8b737ce96f09cee57c50
Leonardo Viana <sonleo2013@gmail.com>
2021-01-22 12:08:22 -0400

    chore: lock device orientation in portrait

--- Instagram-SwiftUI/AppDelegate.swift
+++ Instagram-SwiftUI/AppDelegate.swift
@@ -10,6 +10,12 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    static var orientationLock =
+    UIInterfaceOrientationMask.portrait
+
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
+    }
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
