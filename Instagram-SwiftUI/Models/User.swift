b3e92b25c54f1591ef1330cd86f3c72caa5132cc
Leonardo Viana <sonleo2013@gmail.com>
2021-01-22 22:41:05 -0400

    chore(user): add field username in model

--- Instagram-SwiftUI/Models/User.swift
+++ Instagram-SwiftUI/Models/User.swift
@@ -10,18 +10,19 @@ import Foundation
 struct User {
     var id: String = UUID().uuidString
     var imageName: String = ""
+    var username: String = ""
     
     func all() -> [User] {
         return [
-            User(imageName: "User0"),
-            User(imageName: "User1"),
-            User(imageName: "User2"),
-            User(imageName: "User3"),
-            User(imageName: "User4"),
-            User(imageName: "User5"),
-            User(imageName: "User6"),
-            User(imageName: "User7"),
-            User(imageName: "User8"),
+            User(imageName: "User0", username: "user99"),
+            User(imageName: "User1", username: "anonymous"),
+            User(imageName: "User2", username: "vabrz"),
+            User(imageName: "User3", username: "helybr"),
+            User(imageName: "User4", username: "itsme"),
+            User(imageName: "User5", username: "theauthor"),
+            User(imageName: "User6", username: "p3rs0n"),
+            User(imageName: "User7", username: "eldoe"),
+            User(imageName: "User8", username: "verkz"),
         ]
     }
 }
